3dbe28dff923339190c70a636a12b62057e7b456
NetLSS <futureprogrammer@naver.com>
2021-07-16 15:32:12 +0900

    Feat: fragment view binding 적용

--- app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/PlayerFragment.kt
+++ app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/PlayerFragment.kt
@@ -1,10 +1,29 @@
 package com.lilcode.aop.p4c02.musicstreaming
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.lilcode.aop.p4c02.musicstreaming.databinding.FragmentPlayerBinding
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
+
+    private var _binding: FragmentPlayerBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPlayerBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -12,8 +31,8 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
 
     }
 
-    companion object{
-        fun newInstance(): PlayerFragment{
+    companion object {
+        fun newInstance(): PlayerFragment {
             return PlayerFragment()
         }
     }
