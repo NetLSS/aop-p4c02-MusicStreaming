a067695c5c7dbc28def86ad62c22830b51033748
NetLSS <futureprogrammer@naver.com>
2021-07-16 18:01:48 +0900

    Feat: retrofit 사용을 위한 dto, entity, service 정의

---
+++ app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/service/MusicDto.kt
@@ -0,0 +1,5 @@
+package com.lilcode.aop.p4c02.musicstreaming.service
+
+data class MusicDto (
+    val musics: List<MusicEntity> // 서버에서 받아올 데이터
+)
---
+++ app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/service/MusicService.kt
@@ -0,0 +1,9 @@
+package com.lilcode.aop.p4c02.musicstreaming.service
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MusicService {
+    @GET("/v3/e4db045a-23a9-4b49-a3fc-78cf51f3f964")
+    fun listMusics(): Call<MusicDto>
+}
