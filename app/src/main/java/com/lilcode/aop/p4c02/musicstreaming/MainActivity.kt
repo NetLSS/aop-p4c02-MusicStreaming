a72f063769934d5c21c8535dc8b3afae937bd35e
NetLSS <futureprogrammer@naver.com>
2021-07-16 15:12:05 +0900

    Feat: 뷰바인딩 적용

--- app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/MainActivity.kt
+++ app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/MainActivity.kt
@@ -2,10 +2,17 @@ package com.lilcode.aop.p4c02.musicstreaming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lilcode.aop.p4c02.musicstreaming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _viewBinding: ActivityMainBinding? = null
+    private val viewBinding: ActivityMainBinding get() = requireNotNull(_viewBinding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
     }
 }
