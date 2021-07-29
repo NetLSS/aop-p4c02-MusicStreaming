12459d5be9e66b077c19bfd15912492361faac83
NetLSS <futureprogrammer@naver.com>
2021-07-29 23:27:26 +0900

    Feat: 재생 중에 따라 뷰홀더 배경색 변경 (재생중이면 회색 배경)

--- app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/MusicAdapter.kt
+++ app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/MusicAdapter.kt
@@ -1,5 +1,6 @@
 package com.lilcode.aop.p4c02.musicstreaming
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -9,7 +10,7 @@ import com.bumptech.glide.Glide
 import com.lilcode.aop.p4c02.musicstreaming.databinding.ItemMusicBinding
 import com.lilcode.aop.p4c02.musicstreaming.service.MusicModel
 
-class MusicAdapter: ListAdapter<MusicModel, MusicAdapter.ViewHolder>(diffUtil) {
+class MusicAdapter(private val callback: (MusicModel) -> Unit): ListAdapter<MusicModel, MusicAdapter.ViewHolder>(diffUtil) {
 
     inner class ViewHolder(private val itemMusicBinding: ItemMusicBinding):RecyclerView.ViewHolder(itemMusicBinding.root){
         fun bind(music: MusicModel){
@@ -19,6 +20,18 @@ class MusicAdapter: ListAdapter<MusicModel, MusicAdapter.ViewHolder>(diffUtil) {
             Glide.with(itemMusicBinding.itemCoverImageView.context)
                 .load(music.coverUrl)
                 .into(itemMusicBinding.itemCoverImageView)
+
+            // 재생 중에 따라
+            if(music.isPlaying){
+                // itemView 를 사용했는데 이건 리사이클러 뷰에서 뷰홀더(아이템 하나) 현재 아이템에 해당
+                itemView.setBackgroundColor(Color.GRAY) // 재생중이면 배경 색을 회색
+            }else{
+                itemView.setBackgroundColor(Color.TRANSPARENT)
+            }
+
+            itemView.setOnClickListener {
+                callback(music)
+            }
         }
     }
 
@@ -33,11 +46,11 @@ class MusicAdapter: ListAdapter<MusicModel, MusicAdapter.ViewHolder>(diffUtil) {
 
     companion object{
         val diffUtil = object : DiffUtil.ItemCallback<MusicModel>(){
-            override fun areItemsTheSame(oldItem: MusicModel, newItem: MusicModel): Boolean {
+            override fun areItemsTheSame(oldItem: MusicModel, newItem: MusicModel): Boolean { // id 값만 비교
                 return oldItem.id == newItem.id
             }
 
-            override fun areContentsTheSame(oldItem: MusicModel, newItem: MusicModel): Boolean {
+            override fun areContentsTheSame(oldItem: MusicModel, newItem: MusicModel): Boolean { // 내부 내용 비교
                 return oldItem == newItem
             }
 
