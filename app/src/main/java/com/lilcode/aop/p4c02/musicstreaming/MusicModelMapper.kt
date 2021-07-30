f5c6365385098320360b33768992672e4c4d02e0
NetLSS <futureprogrammer@naver.com>
2021-07-30 15:55:27 +0900

    Feat: MusicDto -> PlayerModel mapper 추가

--- app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/MusicModelMapper.kt
+++ app/src/main/java/com/lilcode/aop/p4c02/musicstreaming/MusicModelMapper.kt
@@ -1,5 +1,6 @@
 package com.lilcode.aop.p4c02.musicstreaming
 
+import com.lilcode.aop.p4c02.musicstreaming.service.MusicDto
 import com.lilcode.aop.p4c02.musicstreaming.service.MusicEntity
 import com.lilcode.aop.p4c02.musicstreaming.service.MusicModel
 
@@ -7,3 +8,9 @@ import com.lilcode.aop.p4c02.musicstreaming.service.MusicModel
 fun MusicEntity.mapper(id: Long): MusicModel =
     MusicModel(id = id, track, streamUrl, artist, coverUrl)
 
+fun MusicDto.mapper(): PlayerModel =
+    PlayerModel(
+        playMusicList = musics.mapIndexed { index, musicEntity ->
+            musicEntity.mapper(index.toLong())
+        }
+    )
